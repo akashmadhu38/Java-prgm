e13f2f42922974b31779cd347c5d159f2304a74e
akashmadhu38 <76092167+akashmadhu38@users.noreply.github.com>
2021-08-22 09:03:51 -0700

    Update CO3PG2.java

--- CO3/CO3PG2.java
+++ CO3/CO3PG2.java
@@ -1,37 +1,42 @@
-package src.CO3;
+
 import java.util.*;
-class Investment{
-    Scanner sc = new Scanner(System.in);
-    double amount,intrest;
-    void intrest(double a){
-        System.out.println("Enter the amount to claculate intrest");
-        amount = sc.nextDouble();
-        intrest = amount*a;
-        System.out.println("Intrest as per amount is:"+intrest);
-    }
-    void menu(){
-        int choice;
-        do{
-            System.out.println("\n1.Fedral Bank\n2.Karur Bank\n3.BOI Bank\n4.Exit\nEnter the choice:");
-            choice = sc.nextInt();
-            switch (choice){
-                case 1:intrest(8.4f/100f);
-                break;
-                case 2:intrest(7.3f/100f);
-                break;
-                case 3:intrest(9.7f/100f);
-                break;
-                case 4:System.exit(0);
-                break;
-                default:System.out.println("INVALID");
-                break;
-            }
-        }while (choice!=4);
+
+class customer{
+    String name,bank;
+    int accno,amnt;
+    customer(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name:");
+        name = sc.next();
+        System.out.println("Enter Bank:");
+        bank = sc.next();
+        System.out.println("Accno:");
+        accno = sc.nextInt();
+        System.out.println("Enter Amount:");
+        amnt = sc.nextInt();
     }
 }
+
+class Investment {
+    static void intrest(customer c){
+        switch (c.bank){
+            case "fedral":System.out.println("Intrest at fedral:"+(c.amnt*(8.4f/100f)));
+                break;
+            case "Karur":System.out.println("Intrest at Karur:"+(c.amnt*(7.3f/100f)));
+                break;
+            case "BOI":System.out.println("Intrest at BOI:"+(c.amnt*(9.7f/100f)));
+                break;
+            default:System.exit(0);
+                break;
+        }
+
+    }
+
+}
+
 public class CO3PG2 {
     public static void main(String ...a){
-        Investment i = new Investment();
-        i.menu();
+        customer c = new customer();
+        Investment.intrest(c);
     }
 }
